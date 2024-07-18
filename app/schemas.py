9aca484e466b06e0c4b90dff55b3865859a128f3
El amrani Amine <amine_el_amrani@outlook.com>
2024-07-18 13:06:08 +0200

    feat: Add Mission and Expense schemas

--- app/schemas.py
+++ app/schemas.py
@@ -8,3 +8,22 @@ class UserSchema(Schema):
     is_manager = fields.Bool(dump_only=True)
 
 user_schema = UserSchema()
+
+class MissionSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(min=1))
+    description = fields.Str()
+    user_id = fields.Int(dump_only=True)
+
+mission_schema = MissionSchema()
+missions_schema = MissionSchema(many=True)
+
+class ExpenseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    amount = fields.Float(required=True)
+    description = fields.Str()
+    date = fields.Date(required=True)
+    mission_id = fields.Int(dump_only=True)
+
+expense_schema = ExpenseSchema()
+expenses_schema = ExpenseSchema(many=True)
