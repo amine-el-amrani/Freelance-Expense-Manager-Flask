f04bb161d88de4b309d8caad1144b73560360ed1
El amrani Amine <amine_el_amrani@outlook.com>
2024-07-18 14:06:42 +0200

    feat: Add TestingConfig for testing environment

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@db/Freelance-Expense-Manager'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    WTF_CSRF_ENABLED = False
