b4544dff370feff2c93355068206b184fcd88ef1
El amrani Amine <amine_el_amrani@outlook.com>
2024-07-18 12:24:57 +0200

    feat: Add User model to the app

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_manager = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_token(self, expires_in=3600):
+        return create_access_token(identity=self.id, expires_delta=timedelta(seconds=expires_in))
