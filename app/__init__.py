bfbaca656af1030b07aeec26c391cce654890833
El amrani Amine <amine_el_amrani@outlook.com>
2024-07-18 12:59:22 +0200

    chore: Update __init__.py to load environment variables with python-dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
